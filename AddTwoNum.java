c37cb8fd7970b5e9142dbce8d7336b84d09a96a0
Abhijjit Mahata <iabhijit97@gmail.com>
2022-11-09 09:57:50 +0530

    initial commit

---
+++ AddTwoNum.java
@@ -0,0 +1,13 @@
+
+public class AddTwoNum
+{
+    public void sumOfNum(int a, int b){
+        int sum = a+b;
+        System.out.println("The Sum is :"+sum);
+
+    }
+    public static void main(String[] arg){
+        AddTwoNum addNum = new AddTwoNum();
+        addNum.sumOfNum(2,4);
+    }
+}

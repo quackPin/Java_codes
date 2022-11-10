b231a1dc98f429039374d5657116d9fcd62a0deb
Abhijjit Mahata <iabhijit97@gmail.com>
2022-11-10 17:47:52 +0530

    first commit

---
+++ OddEvenCheck.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class OddEvenCheck{
+
+    public static void main(String[] args){
+        int num;
+        Scanner scan = new Scanner(System.in);
+        System.out.printf("Enter a number: ");
+        num = scan.nextInt();
+        scan.close();
+
+        String result = (num%2 == 0)? "Even number" : "Odd number";
+        System.out.println(num+" is an "+result);
+    }
+}

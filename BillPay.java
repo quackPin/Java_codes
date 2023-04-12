39211c913199f876eae5c5e68f09c4c0f66c8ab5
Abhijit Mahata <abhijitmahata@Abhijits-MacBook-Air.local>
2023-04-12 23:45:50 +0530

    initial commit

---
+++ BillPay.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class BillPay {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Item one price: ");
+        float num1 = sc.nextFloat();
+        System.out.print("Item two price: ");
+        float num2 = sc.nextFloat();
+        float sum = num1 + num2;
+        sc.close();
+        System.out.println("Subtotal amount: "+sum);
+        System.out.println("Additional 18% GST");
+        float gst = sum * 18 /100;
+        System.out.println("Your GST bill: "+gst);
+        System.out.format("You have to pay: %.2f",gst);
+    }
+    
+}

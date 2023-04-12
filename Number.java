e9cd39cb7f128c5b53b70af395b99e8abb8176f2
Abhijit Mahata <iabhijit97@gmail.com>
2023-04-13 01:34:11 +0530

    first commit

---
+++ Number.java
@@ -0,0 +1,13 @@
+// import java.util.*; 
+
+public class Number {
+    public static void main(String[] args) {
+        for(int i = 9; i<100; i++){
+            if(i%2 != 0){
+                System.out.print(i+" ");
+            }
+        }
+
+    }
+}
+
